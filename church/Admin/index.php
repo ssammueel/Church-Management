2d2fece78e1f91ecc273262787982232ed8ad325
samuel <124765323+ssammueel@users.noreply.github.com>
2024-03-04 10:35:34 +0300

    Add files via upload

---
+++ church/Admin/index.php
@@ -0,0 +1,17 @@
+<?php include('header.php'); ?>
+<body class="body"  id="login">
+    <div class="container">
+		<div class="row-fluid">
+			<div class="span6"><div class="title_index"><?php include('title_index.php'); ?></div></div>
+			<div class="span6"><div class="pull-right"><?php include('login_admin.php'); ?></div></div>
+		</div>
+		<div class="row-fluid">
+          
+	    </div>
+		
+			<?php include('footer.php'); ?>
+    </div>
+<?php include('script.php'); ?>
+</body>
+
+</html>
